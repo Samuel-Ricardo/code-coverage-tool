938bb060f4a2f149b50a4f7070a2164bd0c1e7cd
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-28 19:34:27 -0300

    [ :sparkles: ] | create: coverage - report (app)

--- src/coverage.mjs
+++ src/coverage.mjs
@@ -8,12 +8,44 @@ const CURRENT_FILE_NAME = fileURLToPath(import.meta.url);
 const COLORS = {
   GREEN: "\x1b[32m",
   RED: "\x1b[31m",
-  RESET: "\x1b[0m",
+  END_LINE: "\x1b[0m",
 };
 
 function filterResults(coverage) {
   return coverage.result.filter(({ url }) => {
     const finalUrl = Url.replace("file://", "");
     return isAbsolute(finalUrl) && finalUrl !== CURRENT_FILE_NAME;
+  });
+}
+
+function generateCoverageReport(filename, sourceCode, functions) {
+  const uncoveredLines = [];
+
+  for (const cov of functions) {
+    for (const range of cov.ranges) {
+      if (range.count !== 0) continue;
+
+      const startLine = sourceCode
+        .substring(0, range.startOffset)
+        .split("\n").length;
+
+      const endLine = sourceCode
+        .substring(0, range.endOffset)
+        .split("\n").length;
+
+      for (let charIndex = startLine; charIndex <= endLine; charIndex++) {
+        uncoveredLines.push(charIndex);
+      }
+    }
+  }
+
+  console.log("\n", COLORS.GREEN + filename + COLORS.END_LINE);
+
+  sourceCode.split("\n").forEach((line, lineIndex) => {
+    if (uncoveredLines.includes(lineIndex + 1) && !line.startsWith("}")) {
+      console.log(COLORS.RED + line + COLORS.END_LINE);
+    } else {
+      console.log(line);
+    }
   });
 }
